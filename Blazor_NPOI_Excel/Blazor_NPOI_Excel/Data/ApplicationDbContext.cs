f355a78ed5a5fa68ebdc1e6df42dbc2af7ec68f0
zhusir <smallprogram@foxmail.com>
2021-01-17 16:32:53 +0800

    update

---
+++ Blazor_NPOI_Excel/Blazor_NPOI_Excel/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor_NPOI_Excel.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
