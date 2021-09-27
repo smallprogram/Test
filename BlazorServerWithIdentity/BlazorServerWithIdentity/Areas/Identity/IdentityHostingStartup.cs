2f8b1742a8081f609ea4103036a0c752fffff8bf
zhusir <smallprogram@foxmail.com>
2021-09-27 14:59:21 +0800

    update init BlazerServer Identity

---
+++ BlazorServerWithIdentity/BlazorServerWithIdentity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BlazorServerWithIdentity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BlazorServerWithIdentity.Areas.Identity.IdentityHostingStartup))]
+namespace BlazorServerWithIdentity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
